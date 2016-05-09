4ce0ec64c246e46483d8f32b9e3228271fb5e436
ryotohotta <ryoto_hotta@r.recruit.co.jp>
2016-05-09 18:10:49 +0900

    ログイン周りを修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,15 +4,15 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   def index
-    @articles = Article.all
-    @article = Article.all
+    @articles = Article.includes(:user).all
+    @article = Article.includes(:user).all
   end
   def show
     @article = Article.find(params[:id])
     @talks = @article.talks.includes(:user)
   end
   def create
-    Article.create(article_params)
+    Article.create(image: article_params[:image], category: article_params[:category], comment: article_params[:comment], user_id: current_user.id)
   end
 
   private
